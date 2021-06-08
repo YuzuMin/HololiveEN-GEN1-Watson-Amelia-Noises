bcf7b9799eb5ffb152d5955f765b4e3ccc340526
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-06-08 17:59:23 +0800

    Create CreateAlarmViewModel.java

---
+++ Stable/AmeliaNoises/app/src/main/java/com/yuzumin/amelianoises/createalarm/CreateAlarmViewModel.java
@@ -0,0 +1,23 @@
+package com.yuzumin.amelianoises.createalarm;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+import com.yuzumin.amelianoises.data.Alarm;
+import com.yuzumin.amelianoises.data.AlarmRepository;
+
+public class CreateAlarmViewModel extends AndroidViewModel {
+    private AlarmRepository alarmRepository;
+
+    public CreateAlarmViewModel(@NonNull Application application) {
+        super(application);
+
+        alarmRepository = new AlarmRepository(application);
+    }
+
+    public void insert(Alarm alarm) {
+        alarmRepository.insert(alarm);
+    }
+}
